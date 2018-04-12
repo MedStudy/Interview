75d776f5475a383cd644e3241e9f199619b4d762
kellymccormack <contact@kellymccormack.com>
2018-04-12 13:41:12 -0600

    completed code test

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+
+        //get input from text field
+        var inputText;
+
+        document.getElementsByClassName('mat-button')[0].onclick = function () { 
+            inputText = document.getElementById('input-textbox').value;
+
+            gitHubRequest(inputText);
+        };
+
+        
+        //perform AJAX API request of username results (returns up to 30)
+        function gitHubRequest(username) {
+            var request = new XMLHttpRequest();
+            request.onload = displayResults;
+            request.open('get', 'https://api.github.com/search/users?q=' + username);
+            request.send();
+        }
+        
+        //display results - total #, first 10 users (or any if less than 10)
+        function displayResults() {
+            var responseObj = JSON.parse(this.responseText);
+            
+            document.getElementById('totalcount-container').innerText = 'total count = ' + responseObj.total_count;
+            
+            var iMax = responseObj.items.length > 10 ? 10 : responseObj.items.length;
+            
+            var users = [];
+            for (var i = 0; i < iMax; i++) {
+                console.log(responseObj.items[i].login);
+                if (i == 0) {
+                    document.getElementById('users-container').innerHTML = 'First 10 users = <br />';
+                }
+                document.getElementById('users-container').innerHTML += responseObj.items[i].login + '<br />';
+            }
+        }
+    }
 
 }
